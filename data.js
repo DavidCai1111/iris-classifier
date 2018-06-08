d8e93b77ddf9d6dc12a5ea7cfb2583c7fa106f1d
David Cai <davidcai1993@yahoo.com>
2018-06-08 14:07:42 +0800

    feat: use tfjs

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 'use strict'
 const fs = require('fs')
-const path = require('path')
+const tf = require('@tensorflow/tfjs')
 const csv = require('neat-csv')
 
 async function getTrainData () {
@@ -14,25 +14,22 @@ async function getTestData () {
 async function getData (dataPath) {
   const parsedCSV = await csv(fs.readFileSync(dataPath))
 
-  const inputs = {
-    sepalLength: new Float32Array(parsedCSV.length),
-    sepalWidth: new Float32Array(parsedCSV.length),
-    petalLength: new Float32Array(parsedCSV.length),
-    petalWidth: new Float32Array(parsedCSV.length)
-  }
-
-  const labels = []
+  let inputs = []
+  let labels = []
 
   parsedCSV.forEach((row, i) => {
-    inputs.sepalLength[i] = row['4']
-    inputs.sepalWidth[i] = row['30']
-    inputs.petalLength[i] = row['setosa']
-    inputs.petalWidth[i] = row['versicolor']
+    inputs.push(row['4'], row['30'], row['setosa'], row['versicolor'])
 
-    labels.push(row['virginica'])
+    const label = row['virginica']
+    if (label === 0) labels.push(1, 0, 0)
+    if (label === 1) labels.push(0, 1, 0)
+    else labels.push(0, 0, 1)
   })
 
-  return { inputs, labels }
+  return {
+    inputs: tf.tensor(inputs, [parsedCSV.length, 4]),
+    labels: tf.tensor(labels, [parsedCSV.length, 3])
+  }
 }
 
 module.exports = {
--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 'use strict'
 const Keras = require('keras-js')
+const tf = require('@tensorflow/tfjs')
 const co = require('co')
 const { getTrainData, getTestData } = require('./data')
 
 co(async function () {
+  const model = tf.sequential()
+  model.add(tf.layers.dense({ units: 10, activation: 'relu', inputShape: [4] }))
+  model.add(tf.layers.dense({ units: 10, activation: 'relu' }))
+  model.add(tf.layers.dense({ units: 3, activation: 'softmax' }));
+
+  const optimizer = tf.train.adam(0.001)
+  model.compile({
+    optimizer: optimizer,
+    loss: 'categoricalCrossentropy',
+    metrics: ['accuracy'],
+  })
+
+  const trainData = await getTestData()
+  const testData = await getTestData()
+
+  await model.fit(trainData.inputs, trainData.labels, {
+    epochs: 100,
+    validationData: [testData.inputs, testData.labels]
+  })
+
 }).catch(console.error)
