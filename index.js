936f95d67ffd77f07a5a1cbac6939c81765a9316
David Cai <davidcai1993@yahoo.com>
2018-06-12 09:16:43 +0800

    chore: use standard style

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 'use strict'
-const Keras = require('keras-js')
 const tf = require('@tensorflow/tfjs')
 const co = require('co')
 const { getTrainData, getTestData } = require('./data')
@@ -8,21 +7,23 @@ co(async function () {
   const model = tf.sequential()
   model.add(tf.layers.dense({ units: 10, activation: 'relu', inputShape: [4] }))
   model.add(tf.layers.dense({ units: 10, activation: 'relu' }))
-  model.add(tf.layers.dense({ units: 3, activation: 'softmax' }));
+  model.add(tf.layers.dense({ units: 3, activation: 'softmax' }))
 
   const optimizer = tf.train.adam(0.001)
   model.compile({
     optimizer: optimizer,
     loss: 'categoricalCrossentropy',
-    metrics: ['accuracy'],
+    metrics: ['accuracy']
   })
 
-  const trainData = await getTestData()
+  const trainData = await getTrainData()
   const testData = await getTestData()
 
   await model.fit(trainData.inputs, trainData.labels, {
-    epochs: 100,
+    epochs: 300,
     validationData: [testData.inputs, testData.labels]
   })
 
+  const res = model.predict(testData.inputs[3])
+  console.log(res, testData.labels[3])
 }).catch(console.error)
